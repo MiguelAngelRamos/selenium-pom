f143fe4155c680fc2b4c69a5627621d9311db44f
Miguel Ramos <return.infox@gmail.com>
2025-07-22 19:13:39 -0400

    actualizacion

--- src/test/java/cl/kibernumacademy/pom/tests/WebFormTest.java
+++ src/test/java/cl/kibernumacademy/pom/tests/WebFormTest.java
@@ -23,11 +23,34 @@ public class WebFormTest {
   @BeforeAll
   void setupClass() {
     WebDriverManager.chromedriver().setup();
+    /**
+     *  ¿Qué hace WebDriverManager.chromedriver().setup()?
+        ✅ Descarga y configura automáticamente el ejecutable del ChromeDriver que sea compatible con la versión de Google Chrome instalada en tu máquina.
+        ✅ Establece la ruta del ejecutable en el PATH del sistema (o en una propiedad del sistema) para que cuando tú crees una instancia de new ChromeDriver(), funcione sin que tengas que preocuparte por tener el driver ni por su ubicación.
+     */
   }
 
   @BeforeEach
   void setupTest() {
     driver = new ChromeDriver(); // Abre el navegador Chrome
+    /*
+     * Aunque ya configuraste en @BeforeAll qué driver se usará y dónde está,
+      cada vez que abres un ChromeDriver() creas un navegador nuevo y limpio.
+
+      En otras palabras:
+
+      @BeforeAll → solo descarga y configura el ejecutable de chromedriver.exe y deja el sistema listo.
+
+      new ChromeDriver() → abre una ventana nueva de Chrome controlada por Selenium.
+     */
+
+     /*
+      *  lo habitual (y recomendable) en pruebas automáticas es que cada test tenga su propio navegador independiente.
+      Así:
+      ✅ Si un test falla, no afecta al estado de otro.
+      ✅ Cada prueba empieza con un navegador limpio, sin cookies, sin sesión previa, sin datos de otro test.
+
+      */
     driver.get(URL); // Navega a la URL del formulario web
     webFormPage = new WebFormPage(driver); // Inicializa nuestro Page Object
   }
